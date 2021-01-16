84c49c811de99cad348c949af359944f75f8b69d
Jon Bechtel <jbechtel@indigoag.com>
2021-01-16 13:51:33 -0500

    misc

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import find_packages, setup
 
 VERSION = "0.0.0"
 
@@ -10,12 +10,12 @@ REQUIRED_DEV_PACKAGES = []
 with open("requirements-dev.txt", "r") as reqs_dev_txt_file:
     REQUIRED_DEV_PACKAGES = [line.strip() for line in reqs_dev_txt_file]
 
-setuptools.setup(
+setup(
     name="vibsym", 
     version=VERSION,
     author="Jonathon Bechtel",
     description="A small symmetry package for 2D molecules",
     url="https://github.com/jbechtel/vibsym",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     python_requires='==3.8.1',
 )
