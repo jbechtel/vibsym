e9b1cefa34917965986efb8de7f193112e685614
Jon Bechtel <jbechtel@indigoag.com>
2021-01-16 14:14:57 -0500

    fix logs

--- runs/run_example.py
+++ runs/run_example.py
@@ -1,5 +1,8 @@
 from vibsym.plots import plot_normal_modes
 from vibsym.example_reps import get_example_molecule, ExampleMoleculeType
+import logging
+
+logger = logging.getLogger(__name__)
 
 mol_type = ExampleMoleculeType.SQUARE
 mol = get_example_molecule(mol_type)
--- vibsym/__init__.py
+++ vibsym/__init__.py
@@ -1,13 +1,4 @@
-from __future__ import absolute_import
-from . import repgen
-from . import misc
-from .SymRep import SymRep
-#from .Misc import *
-#from .AsymNet import AsymNet
-#from . import dataIO
-#from .EnergyData import EnergyData
-#from .ForceData import ForceData
-#from .CrystalNet import CrystalNet
-#from .DataManager import DataManager
-#from .RandomChunker import RandomChunker
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
